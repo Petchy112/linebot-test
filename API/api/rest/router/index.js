0382f146069b908254a991e8700464f01e34b4f5
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-17 16:53:15 +0700

    add vote route

--- API/api/rest/router/index.js
+++ API/api/rest/router/index.js
@@ -1,10 +1,13 @@
 const functions = require('./functions');
 const user = require('./user');
 const estimate = require('./estimate');
+const voteResult = require('./voteResult')
 
 
 module.exports = app => {
     app.use('/function', functions)
     app.use('/user', user)
     app.use('/estimate', estimate)
+    app.use('/vote', voteResult)
+   
 }
