15631e3d6cb05c57de3666f1b27bf6a70f8bb751
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-08-19 13:56:43 +0700

    add platform and remove time

--- API/models/functionModel.js
+++ API/models/functionModel.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const TimeResult = require('./timeResultModel');
 
 var choiceSchema = mongoose.Schema({
     name: { type: String, require: true },
     description: { type: String, require: true },
-    time: { type: Number, require: false, default: '', }
-},
-    { _id: false });
+})
 
 var schema = mongoose.Schema({
+    platform: { type: String, enum: ['WEBSITE', 'MOBILE'], require: false },
     group: { type: String, require: true },
     choice: { type: Array.of(choiceSchema), require: true },
     updateAt: { type: Date, default: Date.now() }
