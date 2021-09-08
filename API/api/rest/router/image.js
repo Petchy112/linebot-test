df52009acfaab678abf7ce511149468636283f76
Kittikan Ditkhanan <petch_501_@hotmail.com>
2021-09-08 18:21:10 +0700

    update image API

--- API/api/rest/router/image.js
+++ API/api/rest/router/image.js
@@ -6,10 +6,8 @@ const createError = require('http-errors');
 const path = require('path');
 const upload = require('../../../multer/storage');
 
-
 router.post('/uploadImage', withAuth, upload.single('images'), async (req, res, next) => {
     try {
-        console.log(req);
         const image = req['file']
         const accessToken = req.accessToken
         const upload = await imageService.uploadImage(accessToken, image)
@@ -17,13 +15,12 @@ router.post('/uploadImage', withAuth, upload.single('images'), async (req, res, 
     }
     catch (error) {
         next(error)
+        throw error
     }
 })
-
 router.get('/getImage/:name', async(req, res, next) => {
-    console.log('yes')
 
-    const name = request.params.name
+    const name = req.params.name
     console.log(name)
 
     const options = {
@@ -44,10 +41,8 @@ router.get('/getImage/:name', async(req, res, next) => {
         }
     })
 })
-
 router.get('/getImage', async (req, res,next) => {
     try {
-        
         const { query } = req
         console.log(query.imageId)
 
@@ -59,7 +54,7 @@ router.get('/getImage', async (req, res,next) => {
         if (imageData) {
             const image = {
                 id: imageData.data.id,
-                fullPath: 'http' + '://' + request.get('host') + '/api/getImage/' + imageData.data.name,
+                fullPath: 'http' + '://' + req.get('host') + '/image/getImage/' + imageData.data.name,
             }
 
             res.json(image)
