c61a3f053ab1e8343ad6f59e334f602a0b716d0d
Kittikan Ditkhanan <mju6104101307@mju.ac.th>
2022-05-08 10:44:45 +0700

    fixed bug

--- API/api/rest/index.js
+++ API/api/rest/index.js
@@ -7,33 +7,34 @@ const createError = require('http-errors')
 const multer = require('multer');
 const upload = multer();
 
-app.use(cors())
-app.disable('x-powered-by')
+module.exports = async () => {
+    app.use(cors())
+    app.disable('x-powered-by')
 
-// for parsing multipart/form-data
-app.use(upload.array()); 
-app.use(express.static('public'));
-// for parsing application/json
-app.use(express.json());
-// for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+    // for parsing multipart/form-data
+    app.use(upload.array()); 
+    app.use(express.static('public'));
+    // for parsing application/json
+    app.use(express.json());
+    // for parsing application/x-www-form-urlencoded
+    app.use(express.urlencoded({ extended: true }));
 
-routes(app);
+    routes(app);
 
-app.use((req, res, next) => {
-    next(createError(404, 'Not Found'));
-})
-
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send({
-        error: {
-            status: err.status || 500,
-            message: err.message
-        }
+    app.use((req, res, next) => {
+        next(createError(404, 'Not Found'));
     })
-});
 
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.send({
+            error: {
+                status: err.status || 500,
+                message: err.message
+            }
+        })
+    });
+}
 app.listen(config.PORT, () => {
     console.log(`Server on PORT ${config.PORT}`);
 })
